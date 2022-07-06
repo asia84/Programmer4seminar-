58a6d1e179e6c43cc79ea80c1d1992e0450f0d5c
Asia84 <84asia@mail.ru>
2022-07-06 23:58:24 +0700

    3d пространство, рассчет расстояния между двумя координатами

---
+++ Трехмерное_пространство_расстояние_между_двумя_координатами/Program.cs
@@ -0,0 +1,9 @@
+﻿double x, y, x1, y1, z, z1, len;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+z = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+z1 = Convert.ToDouble(Console.ReadLine());
+len = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1));
+Console.WriteLine(len);
