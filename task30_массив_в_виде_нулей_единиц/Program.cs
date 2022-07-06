33ae082cf79db7ab898a3d37e7ed847ad352b174
Asia84 <84asia@mail.ru>
2022-07-07 02:07:55 +0700

    массив в виде рандомного набора цифр от 0до 1 (8цифр)

---
+++ task30_массив_в_виде_нулей_единиц/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая будет выводить массив из 8 элементов, заполненный нулями и единицами в случайном 
+[1, 0, 1, 1, 0, 0, 0, 1]*/
+
+int[]array = new int[8];//задаем массив. количество элементов. Далее создаем 2 процедуры. процедура всегда void, функция всегда тем типом данных, который будет возвращен 
+void NewArray(int[] collection)// эта процедура определяет коллекцию и
+{
+    int length = collection.Length;//находит ее длину. Можно сразу записать сюда равно 8
+    int index = 0;// начинаем с нуля
+    while(index < length)//пока индекс меньше длины
+    {
+        collection[index] = new Random().Next(0, 2);//присваиваем индексу рандомное число от 0 до 1
+        index++;//плюсуем индес+1
+
+    }
+}
+void PrintArray(int[] col)//вторая процедура выводит результат
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+NewArray(array);
+PrintArray(array);
