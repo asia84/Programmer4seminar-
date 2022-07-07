85d03ed28d6cb3879f600f4e1284b0c75f7d20d5
Asia84 <84asia@mail.ru>
2022-07-07 18:35:19 +0700

    возведение в степень числа

---
+++ task25_возведение_в_степень_числа/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число A, B:  ");//сообщение для пользователя, ввести числа
+
+int product = 1;//произведение чисел = 1
+int i = 1;
+int number1 = Convert.ToInt32(Console.ReadLine());//преобразует первое число
+
+int numder2 = Convert.ToInt32(Console.ReadLine());//преобразует второе число
+
+
+while(i <= numder2)//пока счетчик<=второго числа
+{
+    product = number1 * product;//умножаем первое число 
+    i++;//плюсуем счетчик
+} 
+Console.WriteLine($"Число А в степени В будет равно {product}");//выводим на экран ответ
